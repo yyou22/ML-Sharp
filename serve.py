3dc8c9c91e3bfaeb6c0752b8fad9fa45c339bfe6
vyou <vyou@umich.edu>
2026-06-25 16:38:44 -0400

    initial commit

---
+++ serve.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import http.server
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header("Cross-Origin-Opener-Policy", "same-origin")
+        self.send_header("Cross-Origin-Embedder-Policy", "require-corp")
+        super().end_headers()
+
+if __name__ == "__main__":
+    http.server.test(HandlerClass=Handler, port=8000)
